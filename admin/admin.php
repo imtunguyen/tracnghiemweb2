5ad4194ad94f6c113ec4be35525bd13d11c0d6a5
Khoa Nguyen <khoanguyen.coder@gmail.com>
2024-02-21 15:29:13 +0700

    admin page

---
+++ admin/admin.php
@@ -0,0 +1,10 @@
+<?php
+include('../includes/admin_header.php');
+?>
+
+<div class="container">
+    <img src="../images/admin.jpg" style="width: 100%;">
+</div>
+
+<?php
+include('../includes/admin_footer.php');
