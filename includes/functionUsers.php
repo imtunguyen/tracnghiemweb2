7c6524d4898b158a5e32154fd12d4164a4b5d451
Khoa Nguyen <khoanguyen.coder@gmail.com>
2024-03-27 17:03:26 +0700

    dangky

--- includes/functionUsers.php
+++ includes/functionUsers.php
@@ -1,24 +1,26 @@
 <?php
 
-function getUsername($connect, $usernameOrEmail) {
+function getUsername($connect, $usernameOrEmail)
+{
     $stm = $connect->prepare('SELECT * FROM users WHERE username = ? OR email = ?');
-    $stm->bind_param('ss', $usernameOrEmail, $usernameOrEmail); 
+    $stm->bind_param('ss', $usernameOrEmail, $usernameOrEmail);
     $stm->execute();
     $result = $stm->get_result();
     $stm->close();
     return $result;
 }
 
-function updatePassword($connect, $usernameOrEmail, $password){
+function updatePassword($connect, $usernameOrEmail, $password)
+{
     $stm = $connect->prepare('UPDATE users SET mat_khau = ? WHERE username = ? OR email = ?');
     $stm->bind_param('sss', $password, $usernameOrEmail, $usernameOrEmail);
     $stm->execute();
     $stm->close();
 }
 
-function random_number($length = 6) {
-    $min = pow(10, $length -1);
-    $max = pow(10,$length)-1;
+function random_number($length = 6)
+{
+    $min = pow(10, $length - 1);
+    $max = pow(10, $length) - 1;
     return rand($min, $max);
 }
-
