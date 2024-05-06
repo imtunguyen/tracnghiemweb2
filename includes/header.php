e1a773e3e48625619e5afba40f3fa33281060533
Luan <nguyenhuuliem10@gmail.com>
2024-05-06 19:49:32 +0700

    mua

--- includes/header.php
+++ includes/header.php
@@ -30,10 +30,10 @@ include('database.php');
 
       <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
         <li><a href="#" class="nav-link px-2 link-secondary">Home</a></li>
-        <li><a href="#" class="nav-link px-2">Câu hỏi</a></li>
-        <li><a href="lophoc.php" class="nav-link px-2">Lớp học</a></li>
-        <li><a href="#" class="nav-link px-2">Đề thi</a></li>
-        <li><a href="ketquahoctap.php" class="nav-link px-2">Kết quả học tập</a></li>
+        <li><a href="../giaovien/cauhoi.php" class="nav-link px-2">Câu hỏi</a></li>
+        <li><a href="../lophoc.php" class="nav-link px-2">Lớp học</a></li>
+        <li><a href="../giaovien/dethi.php" class="nav-link px-2">Đề thi</a></li>
+        <li><a href="../ketquahoctap.php" class="nav-link px-2">Kết quả học tập</a></li>
       </ul>
 
       <div class="col-md-3 text-end">
