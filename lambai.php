7c3e07af9dda44192427192d213be9a5ec9c5271
Khoa Nguyen <khoanguyen.coder@gmail.com>
2024-05-06 08:58:05 +0700

    set time submit

--- lambai.php
+++ lambai.php
@@ -2,14 +2,44 @@
 
 include('includes/header.php');
 include('includes/database.php');
-if(isset($_POST['ma_bai_thi']) && isset($_POST['ma_de_thi'])) {
+if(isset($_POST['ma_bai_thi']) && isset($_POST['ma_de_thi']) &&
+isset($_POST['thoi_gian_lam_bai']) && isset($_POST['ten_de_thi'])) {
     $ma_de_thi = $_POST['ma_de_thi'];
     $ma_bai_thi = $_POST['ma_bai_thi'];
+    $thoi_gian_lam_bai = $_POST['thoi_gian_lam_bai'];
+    $ten_de_thi = $_POST['ten_de_thi'];
 }
 ?>
 
 
 <div class="container">
+    <h1 class="text-center">
+        <?php echo $ten_de_thi; ?>
+    </h1>
+    <div class="row">
+        <div class="col-8">
+            <div class="text-danger mb-2">
+                <span style="font-weight: bold;">Số lần vi phạm (chuyển tab): </span> 
+                <span>2</span>
+            </div>
+            <div class="mb-2">
+                <span style="font-weight: bold;">Thời gian làm bài: </span>
+                <span><?php echo $thoi_gian_lam_bai .":00"; ?></span>
+            </div>
+            <div class="mb-2">
+                <span style="font-weight: bold;">Thời gian còn lại: </span>
+                <span  id="time"><?php echo $thoi_gian_lam_bai .":00"; ?></span>
+            </div>
+        </div>
+        <div class="col-4" >
+        <ul>
+        <div class="text-danger mb-2" style="font-weight: bold;">Quy định làm bài</div>
+            <li>Không được tab ra trang khác hay ứng dụng khác.</li>
+            <li>Nếu tab quá 3 lần sẽ tự động nộp bài.</li>
+        </ul>
+        </div>
+    </div>
+    
     <form action="ket_qua.php" method = "POST">
     <?php 
     $query = "select ch.* from chi_tiet_de_thi ctdt 
@@ -91,12 +121,49 @@ if(isset($_POST['ma_bai_thi']) && isset($_POST['ma_de_thi'])) {
 
     echo '</div>';
     ?>
+    <input type="text" hidden name="ma_bai_thi" value=" <?php echo $ma_bai_thi; ?> ">
     <div class="button-submit">
-        <input class="btn btn-primary" type="submit" name="submit" value = "Submit">
+        <input class="btn btn-primary" id="submitButton" type="submit" name="submit" value = "Submit">
     <div>
 </form>
 </div>
 
+<script>
+    let time = <?php echo $thoi_gian_lam_bai; ?>;
+    function startCountdown() {
+    var countdownElement = document.getElementById("time");
+    var timeLeft = <?php echo $thoi_gian_lam_bai; ?> * 60;
+
+    var countdownInterval = setInterval(function() {
+        // Chuyển thời gian còn lại thành phút và giây
+        var minutes = Math.floor(timeLeft / 60);
+        var seconds = timeLeft % 60;
+
+        // Định dạng thời gian hiển thị (phút:giây)
+        var formattedTime = minutes.toString().padStart(2, '0') + ":" + seconds.toString().padStart(2, '0');
+        
+        // Hiển thị thời gian còn lại trong phần tử HTML
+        countdownElement.textContent = formattedTime;
+
+        // Giảm thời gian còn lại
+        timeLeft--;
+
+        // Kiểm tra nếu thời gian còn lại là âm hoặc bằng 0, dừng đếm ngược
+        if (timeLeft < 0) {
+            clearInterval(countdownInterval);
+            document.getElementById("submitButton").click();
+        }
+    }, 1000); // Cập nhật thời gian mỗi giây
+}
+
+document.getElementById("submitButton").addEventListener('click', function() {
+
+})
+
+window.onload = function() {
+    startCountdown();
+};
+</script>
 
 <?php
     include('includes/footer.php');
