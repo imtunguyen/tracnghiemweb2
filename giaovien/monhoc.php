9a68fe3cbc51580e9eca27ab6a3b35e03f23cbeb
Khoa Nguyen <khoanguyen.coder@gmail.com>
2024-05-13 11:03:58 +0700

    bootstrap

--- giaovien/monhoc.php
+++ giaovien/monhoc.php
@@ -17,12 +17,7 @@ if(isset($_GET['delete'])){
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="../css/bootstrap.min.css">
-        <link rel="stylesheet" href="../css/style.css">
-
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <link rel="stylesheet" href="https://unpkg.com/placeholder-loading/dist/css/placeholder-loading.min.css">
-    <script src="../js/script.js"></script>
+       
 </head>
 <body>
 <style>
